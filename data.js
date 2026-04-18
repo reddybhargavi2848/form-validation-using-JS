2ddaeecb5e0b3b5eafe4ffa8982fa137b6844cb6
reddybhargavi2848 <reddybhargavi2004@gmail.com>
2026-04-18 10:21:35 +0530

    config: added formvalidation

---
+++ data.js
@@ -0,0 +1,26 @@
+export const users = [
+    {
+        id: 1,
+        name: "bhagi",
+        email: "bhagi@gmail.com",
+        passkey: "radhika"
+    },
+    {
+        id: 2,
+        name: "sai",
+        email: "sai@gmail.com",
+        passkey: "laxmi"
+    },
+    {
+        id: 3,
+        name: "sandy",
+        email: "sandy@gmail.com",
+        passkey: "chowdary"
+    },
+    {
+        id: 4,
+        name: "ramesh",
+        email: "ramesh@gmail.com",
+        passkey: "reddy"
+    },
+]
